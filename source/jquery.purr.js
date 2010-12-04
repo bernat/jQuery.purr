c9f1c164f3d7ec37ccf6000843b84f4b31639e10
Bernat Farrero <bernat@itnig.net>
2010-12-04 19:54:04 +0100

    Adding feature of cancelling popups with ESC key

--- source/jquery.purr.js
+++ source/jquery.purr.js
@@ -21,9 +21,9 @@
                             default: false
  */
 
-( function( $ ) {
+(function($) {
 
-  $.purr = function (notice, options)
+  $.purr = function(notice, options)
   {
     // Convert notice to a jQuery object
     notice = $(notice);
@@ -32,46 +32,45 @@
     notice.addClass('purr');
 
     // Get the container element from the page
-    var cont = document.getElementById( 'purr-container' );
+    var cont = document.getElementById('purr-container');
 
     // If the container doesn't yet exist, we need to create it
-    if ( !cont )
+    if (!cont)
     {
       cont = '<div id="purr-container"></div>';
     }
 
     // Convert cont to a jQuery object
-    cont = $( cont );
+    cont = $(cont);
 
     // Add the container to the page
-    $( 'body' ).append( cont );
+    $('body').append(cont);
 
     notify();
 
     function notify ()
     {
       // Set up the close button
-      var close = document.createElement( 'a' );
-      $( close ).attr(
-        {
+      var close = document.createElement('a');
+      $(close).attr({
           className: 'close',
           href: '#close'
-        }
-      )
-        .appendTo( notice )
-          .click( function ()
-            {
+          }).appendTo(notice).click(function() {
               removeNotice();
-
               return false;
-            }
-          );
+          });
+
+      // If ESC is pressed remove notice
+      $(document).keyup(function(e) {
+        if (e.keyCode == 27) {
+          removeNotice();
+        }
+      });
 
       // Add the notice to the page and keep it hidden initially
-      notice.appendTo( cont )
-        .hide();
+      notice.appendTo(cont).hide();
 
-      if ( jQuery.browser.msie && options.usingTransparentPNG )
+      if (jQuery.browser.msie && options.usingTransparentPNG)
       {
         // IE7 and earlier can't handle the combination of opacity and transparent pngs, so if we're using transparent pngs in our
         // notice style, we'll just skip the fading in.
@@ -80,69 +79,56 @@
       else
       {
         //Fade in the notice we just added
-        notice.fadeIn( options.fadeInSpeed );
+        notice.fadeIn(options.fadeInSpeed);
       }
 
       // Set up the removal interval for the added notice if that notice is not a sticky
-      if ( !options.isSticky )
+      if (!options.isSticky)
       {
-        var topSpotInt = setInterval( function ()
-        {
+        var topSpotInt = setInterval(function() {
           // Check to see if our notice is the first non-sticky notice in the list
-          if ( notice.prevAll( '.purr' ).length == 0 )
+          if (notice.prevAll('.purr').length == 0)
           {
             // Stop checking once the condition is met
-            clearInterval( topSpotInt );
+            clearInterval(topSpotInt);
 
             // Call the close action after the timeout set in options
-            setTimeout( function ()
-              {
+            setTimeout(function() {
                 removeNotice();
-              }, options.removeTimer
-            );
+              }, options.removeTimer);
           }
-        }, 200 );
+        }, 200);
       }
     }
 
-    function removeNotice ()
+    function removeNotice()
     {
       // IE7 and earlier can't handle the combination of opacity and transparent pngs, so if we're using transparent pngs in our
       // notice style, we'll just skip the fading out.
-      if ( jQuery.browser.msie && options.usingTransparentPNG )
+      if (jQuery.browser.msie && options.usingTransparentPNG)
       {
-        notice.css( { opacity: 0  } )
-          .animate(
-            {
-              height: '0px'
-            },
+        notice.css({ opacity: 0 }).animate({ height: '0px'},
             {
               duration: options.fadeOutSpeed,
-              complete:  function ()
-                {
-                  notice.remove();
-                }
+              complete: function ()
+              {
+                notice.remove();
               }
+            }
           );
       }
       else
       {
         // Fade the object out before reducing its height to produce the sliding effect
-        notice.animate(
-          {
-            opacity: '0'
-          },
+        notice.animate({ opacity: '0' },
           {
             duration: options.fadeOutSpeed,
             complete: function ()
               {
-                notice.animate(
-                  {
-                    height: '0px'
-                  },
+                notice.animate({ height: '0px' },
                   {
                     duration: options.fadeOutSpeed,
-                    complete: function ()
+                    complete: function()
                       {
                         notice.remove();
                       }
@@ -155,7 +141,7 @@
     };
   };
 
-  $.fn.purr = function ( options )
+  $.fn.purr = function(options)
   {
     options = options || {};
     options.fadeInSpeed = options.fadeInSpeed || 500;
@@ -164,7 +150,7 @@
     options.isSticky = options.isSticky || false;
     options.usingTransparentPNG = options.usingTransparentPNG || false;
 
-    this.each( function()
+    this.each(function()
       {
         new $.purr( this, options );
       }
